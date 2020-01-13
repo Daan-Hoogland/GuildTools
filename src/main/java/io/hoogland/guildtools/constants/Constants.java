27a506f4076fdd381ea260f62dccc028ec43fded
Dan <Daan@hoogland.io>
2020-01-13 01:47:42 +0100

    Fix date format

--- src/main/java/io/hoogland/guildtools/constants/Constants.java
+++ src/main/java/io/hoogland/guildtools/constants/Constants.java
@@ -13,7 +13,7 @@ public class Constants {
     public static final String COLOR_NOT_OK = "ff0000";
 
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    public static final DateTimeFormatter DATE_TIME_FORMATTER_DATE = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    public static final DateTimeFormatter DATE_TIME_FORMATTER_DATE = DateTimeFormatter.ofPattern("dd-MM-yyyy");
     public static final DateTimeFormatter DATE_TIME_FORMATTER_TIME = DateTimeFormatter.ofPattern("HH:mm");
     public static final DateTimeFormatter DATE_TIME_FORMATTER_PRECISE = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
